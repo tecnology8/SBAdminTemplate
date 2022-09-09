615caf249622139e5c45187e71d645c7aa3b055c
Ruddy Enmanuel Cruceta Mateo <ruddycruceta@hotmail.com>
2022-09-09 08:28:28 -0400

    Add project files.

---
+++ SBAdminTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SBAdminTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
